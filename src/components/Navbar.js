6cf865ee399836c2e3aa8157bd2dafb4d8cdab28
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-01 14:14:55 -0400

    Fix mobile menu closing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Bruins Ultimate <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
