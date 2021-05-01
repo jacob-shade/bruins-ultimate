ac851c5a84f72cf434d7ef94892656ce4e55270f
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-01 14:11:23 -0400

    Fix sign up button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
 import './Navbar.css';
@@ -16,7 +16,11 @@ function Navbar() {
     } else {
       setButton(true);
     }
-  }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
 
   window.addEventListener('resize', showButton);
 
