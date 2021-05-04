13441d0fcca9520cebf0dbe7d7638c39bcf05217
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-04 13:49:54 -0400

    Make Card Item

---
+++ src/components/CardItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function CardItem() {
+  return (
+    <>
+      <li className="cards--item">
+        <Link className="cards--item--link">
+          <figure className="cards--item--pic-wrap">
+            <img src="/" alt="img" className="cards--item--img"/>
+          </figure>
+          <div className="cards--item--info">
+            <h5 className="cards--item--text" />
+          </div>
+        </Link>
+      </li> 
+    </>
+  )
+}
