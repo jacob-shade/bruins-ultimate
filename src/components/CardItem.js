6297429e633e89e4e89d9b71121e9b04c45b458f
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-04 16:14:37 -0400

    Fix Card Images

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function CardItem() {
+function CardItem(props) {
   return (
     <>
       <li className="cards--item">
-        <Link className="cards--item--link">
-          <figure className="cards--item--pic-wrap">
-            <img src="/" alt="img" className="cards--item--img"/>
+        <Link className="cards--item--link" to={props.path}>
+          <figure className="cards--item--pic-wrap" data-category={props.label}>
+            <img
+              src={process.env.PUBLIC_URL + props.src}
+              alt="ultimate game being played"
+              className="cards--item--img"
+            />
           </figure>
           <div className="cards--item--info">
-            <h5 className="cards--item--text" />
+            <h5 className="cards--item--text">{props.text}</h5>
           </div>
         </Link>
-      </li> 
+      </li>
     </>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import CardItem from './CardItem';
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
 
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out how we did in the Fairfax Invite!</h1>
+      <h1>Check out how we did in these recent events!</h1>
       <div className="cards--container">
         <div className="cards--wrapper">
           <ul className="card--items">
-            <CardItem />
+            <CardItem
+              src="images/sign_up.png"
+              text="Open team played strong throughout the tournament"
+              label="Fairfax Invite 2021"
+              path="/about"
+            />
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Cards;
