5fcc5de564709614717714e474bd0a3268a514ed
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-04 13:27:12 -0400

    Change buttons

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,22 +6,22 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1>BRUINS ULTIMATE</h1>
+      <p>Come join the team!</p>
       <div className="hero-btns">
         <Button
           className="btns"
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          GET STARTED
+          ABOUT US
         </Button>
         <Button
           className="btns"
           buttonStyle="btn--primary"
           buttonSize="btn--large"
         >
-          WATCH TRAILER
+          WATCH PROMO
         </Button>
       </div>
     </div>
