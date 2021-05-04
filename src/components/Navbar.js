eb85881b7be9719273706807bff4ba5e74a51b75
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-04 13:33:25 -0400

    Change logo to paw

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            Bruins Ultimate <i className="fab fa-typo3" />
+            Bruins Ultimate <i className="fas fa-paw" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
