8307eb07eef42161785869574cbc38b57d0899cf
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-06 10:31:37 -0400

    Remove unused constants

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
 
-const STYLES = ["btn--primary", "btn--outline", "btn--dark"];
-
-const SIZES = ["btn--medium", "btn--large"];
-
 export const Button = ({
   children,
   type,
