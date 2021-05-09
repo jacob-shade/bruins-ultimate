779c4eaaa56e1cd0f3987cbd302a008ea610a048
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-09 09:28:57 -0400

    Update Table templates

--- src/components/RosterTable.jsx
+++ src/components/RosterTable.jsx
@@ -1,53 +1,69 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 500,
   },
 });
 
-function createData(number, player, pronouns, position, role) {
-  return { number, player, pronouns, position, role };
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(number, player, pronouns, grade, role) {
+  return { number, player, pronouns, grade, role };
 }
 
-const rows = [
-  createData("03/03/21", "2:00PM", "James Maddison", "Oak Mar Rec Center", "5-6", "L"),
-];
+const rows = [createData("25", "Nolan Smith", "he/him", "12", "C")];
 
-export default function BasicTable() {
+export default function CustomizedTables() {
   const classes = useStyles();
 
   return (
     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
+      <Table className={classes.table} aria-label="cusomized table">
         <TableHead>
           <TableRow>
-            <TableCell>Number</TableCell>
-            <TableCell align="center">Player</TableCell>
-            <TableCell align="center">Pronouns</TableCell>
-            <TableCell align="center">Position</TableCell>
-            <TableCell align="center">Role</TableCell>
+            <StyledTableCell>No.</StyledTableCell>
+            <StyledTableCell align="center">Player</StyledTableCell>
+            <StyledTableCell align="center">Pronouns</StyledTableCell>
+            <StyledTableCell align="center">Grade</StyledTableCell>
+            <StyledTableCell align="center">Role</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
                 {row.number}
-              </TableCell>
-              <TableCell align="center">{row.player}</TableCell>
-              <TableCell align="center">{row.pronouns}</TableCell>
-              <TableCell align="center">{row.position}</TableCell>
-              <TableCell align="center">{row.role}</TableCell>
-            </TableRow>
+              </StyledTableCell>
+              <StyledTableCell align="center">{row.player}</StyledTableCell>
+              <StyledTableCell align="center">{row.pronouns}</StyledTableCell>
+              <StyledTableCell align="center">{row.grade}</StyledTableCell>
+              <StyledTableCell align="center">{row.role}</StyledTableCell>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
--- src/components/ScheduleTable.jsx
+++ src/components/ScheduleTable.jsx
@@ -1,55 +1,73 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 300,
   },
 });
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
 
 function createData(date, time, opponent, location, score, outcome) {
   return { date, time, opponent, location, score, outcome };
 }
 
 const rows = [
-  createData("03/03/21", "2:00PM", "James Maddison", "Oak Mar Rec Center", "5-6", "L"),
+  createData("03/03", "2:00PM", "James Maddison", "Oak Mar Rec Center", "5-6", "L"),
 ];
 
-export default function BasicTable() {
+export default function CustomizedTables() {
   const classes = useStyles();
 
   return (
     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
+      <Table className={classes.table} aria-label="cusomized table">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell align="center">Time</TableCell>
-            <TableCell align="center">Opponent</TableCell>
-            <TableCell align="center">Location</TableCell>
-            <TableCell align="center">Score</TableCell>
-            <TableCell align="center">Outcome</TableCell>
+            <StyledTableCell>Date</StyledTableCell>
+            <StyledTableCell align="center">Time</StyledTableCell>
+            <StyledTableCell align="center">Opponent</StyledTableCell>
+            <StyledTableCell align="center">Location</StyledTableCell>
+            <StyledTableCell align="center">Score</StyledTableCell>
+            <StyledTableCell align="center">Outcome</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
                 {row.date}
-              </TableCell>
-              <TableCell align="center">{row.time}</TableCell>
-              <TableCell align="center">{row.opponent}</TableCell>
-              <TableCell align="center">{row.location}</TableCell>
-              <TableCell align="center">{row.score}</TableCell>
-              <TableCell align="center">{row.outcome}</TableCell>
-            </TableRow>
+              </StyledTableCell>
+              <StyledTableCell align="center">{row.time}</StyledTableCell>
+              <StyledTableCell align="center">{row.opponent}</StyledTableCell>
+              <StyledTableCell align="center">{row.location}</StyledTableCell>
+              <StyledTableCell align="center">{row.score}</StyledTableCell>
+              <StyledTableCell align="center">{row.outcome}</StyledTableCell>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
