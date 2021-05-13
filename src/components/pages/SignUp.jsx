3c53ec0bb713649ed8c79d0203d0967cb302e57c
Jacob Shade <52800811+shadejacob@users.noreply.github.com>
2021-05-13 11:13:36 -0400

    Add google form to sign up

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import '../../App.css';
+import React from "react";
+import "../../App.css";
+import "./SignUp.css";
 
 export default function SignUp() {
-  return <h1 className="sign-up">sign up</h1>;
+  return (
+    <>
+      <div className="sign-up-form">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc7XYMVX_BqwkEr8Cal4-NehnckbCCS2X6xvU6qz8XAhXw9MQ/viewform?embedded=true">
+          Loading…
+        </iframe>
+      </div>
+    </>
+  );
 }
