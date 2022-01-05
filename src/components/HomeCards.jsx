0c61ca0eff0dcc66792e017d660b511bb4c40a36
Jacob Shade <shadejacob@gmail.com>
2022-01-05 14:07:47 -0500

    update text for recent events

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -11,14 +11,14 @@ function HomeCards() {
           <ul className="cards--items">
             <CardItem
               src="images/sign_up.png"
-              text="Open Team Played Strong Throughout the Tournament"
-              label="Fairfax Invite 2021"
+              text="Finding Our Flow at the Fairfax Finale"
+              label="Fairfax Finale 2021"
               path="/about"
             />
             <CardItem
               src="images/charlie_catch.PNG"
-              text="Bruins Finish 2nd at Mixed 5v5 Tournament"
-              label="Hanover Ultimate Classic 2021"
+              text="Competed in First Ever VA Mixed States"
+              label="VA Mixed States"
               path="/about"
             />
           </ul>
