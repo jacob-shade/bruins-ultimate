58e79e5b98b9a7ee97818fc9de6c7877421f6410
Jacob Shade <shadejacob@gmail.com>
2022-01-06 13:02:45 -0500

    fix dropdown menu responsiveness

--- src/components/pages/Schedule.jsx
+++ src/components/pages/Schedule.jsx
@@ -12,7 +12,7 @@ import F21XSchedule from '../F21XSchedule';
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120,
+    minWidth: 80,
   }
 }))
 
