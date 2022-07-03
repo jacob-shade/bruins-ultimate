638a1c9e52c756fba1c81f2e272ac779b3290b2c
Jacob Shade <shadejacob@gmail.com>
2022-07-03 16:18:51 -0400

    added home, dues, donate pages

---
+++ src/components/pages/Donate.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutBlurbSection from "../AboutBlurbSection";
+import AboutCards from "../AboutCards";
+
+export default function Donate() {
+  return (
+    <>
+      <AboutBlurbSection />
+      {/* <AboutCards /> */}
+    </>
+  );
+}
---
+++ src/components/pages/Dues.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutBlurbSection from "../AboutBlurbSection";
+import AboutCards from "../AboutCards";
+
+export default function Dues() {
+  return (
+    <>
+      <AboutBlurbSection />
+      {/* <AboutCards /> */}
+    </>
+  );
+}
